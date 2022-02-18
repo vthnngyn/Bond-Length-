e46548532b3e548c95082f41742227a7467ec452
vthnngyn <99048734+vthnngyn@users.noreply.github.com>
2022-02-17 23:14:27 -0700

    Fixed comments

--- bl.py
+++ bl.py
@@ -3,7 +3,8 @@ import numpy as np
 import statistics 
 from statistics import mean
 
-a = np.genfromtxt('coords.xyz', delimiter=None, dtype=('str'), unpack=True, usecols=[0])    #xyz file to be read   
+#xyz file to be read 
+a = np.genfromtxt('coords.xyz', delimiter=None, dtype=('str'), unpack=True, usecols=[0])      
 x, y, z = np.genfromtxt('coords.xyz', delimiter=None, unpack=True, usecols=[1, 2, 3])
 
 N = len(a)
@@ -21,7 +22,8 @@ for i in range(0,N-1):
         dy = y[j] - y[i]
         dz = z[j] - z[i]
         L = round(math.sqrt(dx**2 + dy**2 + dz**2),2)
-        if L < int(3):                                                                      #desired upper limit for bond length
+        #desired upper limit for bond length L
+        if L < int(3):                                                                      
              print(a[i],a[j],a_a[i],a_a[j],L) 
              bond.append(L)
              coord.append(a_a[i])
@@ -30,7 +32,8 @@ for i in range(0,N-1):
 print("Average bond length is", round(mean(bond),2))
 print("Standard deviation is", round(statistics.stdev(bond),2))
 
-for i in range(1,N+1):                                                                      #counting number of atoms a atom is coordinated to 
+#coordination number determined by counting the bonds for an atom to all other atoms that are under L 
+for i in range(1,N+1):                                                                      
      print(a[i-1],i,coord.count(i)+coordtwo.count(i))
      if coord.count(i)+coordtwo.count(i) < int(12):    
           CN.append(coord.count(i)+coordtwo.count(i))
